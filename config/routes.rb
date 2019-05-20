e32d3c7de870548074950fe0c98103ce5314455e
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-05-20 19:55:56 +0300

    Add versioning in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  #API definition
+  # Api definition
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' },
                   path: '/' do
-    #We are going to list our resources here
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
+
   end
+  
 end
